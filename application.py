61f79d922f0b3dfedb27664e68a975dc2af44acc
ru chun cui <ruchuncui@ruchundeMacBook-Pro.local>
2018-11-08 21:31:06 -0500

    upload file

--- application.py
+++ application.py
@@ -1,13 +1,45 @@
-from flask import Flask
+from flask import Flask, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
 
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
+ALLOWED_EXTENSIONS = set(['csv'])
 
-@application.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@application.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        up_file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if up_file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+        return '''
+            <!doctype html>
+            <title>Upload new File</title>
+            <h1>Upload new File</h1>
+            <form method=post enctype=multipart/form-data>
+              <input type=file name=file>
+              <input type=submit value=Upload>
+            </form>
+            '''
 
 
 # run the app.
